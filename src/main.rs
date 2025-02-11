7f49d1ef8d866ee5694cba13b758498f533e8587
yifaaan <0219lyf@gmail.com>
2025-02-11 10:28:16 +0800

    init repo

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    /// Initialize a new project
+    Init,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.cmd {
+        Commands::Init => {
+            fs::create_dir(".git").unwrap();
+            fs::create_dir(".git/objects").unwrap();
+            fs::create_dir(".git/refs").unwrap();
+            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            println!("Initialized git repository in current directory.");
+        }
+    }
+}
